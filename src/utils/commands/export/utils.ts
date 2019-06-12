f6457fb91493e579a90ffe443d07ec657546aaf2
Schmidely Stéphane <stephanichous@hotmail.fr>
2019-06-13 00:14:51 +0200

    Implement format tracking

--- src/utils/commands/export/utils.ts
+++ src/utils/commands/export/utils.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment'
 import { EOL } from 'os'
 
 export default class ExportUtils {
@@ -11,6 +12,31 @@ export default class ExportUtils {
     return Object.keys(flags).filter((el, _index, _array) => {
       return el !== 'help'
     })
+  }
+
+  /**
+   * Format timetracking to standart format
+   */
+  public formatTimetracking(tracking: any) {
+    // {
+    //  "start":"20190612T150556Z",
+    //  "end":"20190612T170000Z",
+    //  "tags":["#sideproject","Task/time warrior export"]
+    // }
+    const startDate = moment(tracking.start)
+    const endDate = moment(tracking.end)
+    const duration = moment.duration(endDate.diff(startDate))
+    const formatDatetime = 'DD/MM/YYYY HH:mm'
+
+    const [project, description, ...tags] = tracking.tags
+    return {
+      start: startDate.format(formatDatetime),
+      end: endDate.format(formatDatetime),
+      duration: 'FIXME duration method format HH:mm:'
+      project,
+      description
+    }
+
   }
 
   /**
